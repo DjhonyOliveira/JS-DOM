0e09f8c018d07730869ba591ecebbeea04330dbe
DjhonyMaga <djonatandeoliveira@hotmail.com>
2023-05-23 21:52:40 -0300

    ajustesCss

--- Atividade 03/index.js
+++ Atividade 03/index.js
@@ -2,13 +2,6 @@ const init = document.querySelector('#init')
 const span = document.querySelectorAll('span')
 
 
-span.forEach(function(quadro){
-    quadro.addEventListener('click', function(){  
-        quadro.innerText = "X"
-        quadro.style.color = "#ff0000"
-    })
-})
-
 let playersGame = []
 
 init.addEventListener('click', function(ev){
@@ -22,10 +15,28 @@ init.addEventListener('click', function(ev){
     const playerTwo = document.getElementById('player-two').value
     const peao2 = document.querySelector('.player2 .peao-player .peao:checked').value
 
+    if (peao1.length = 2){
+        alert('Selecione apenas um peão por jogador!!')
+    } else if (peao2.length = 2) {
+        alert('Selecione apenas um peão por jogador!!')
+    }
+
     // push array
     playersGame.push({playerOne, peao1}, {playerTwo, peao2})
 })
 
+
+span.forEach(function(quadro){
+    quadro.addEventListener('click', function(){
+
+        if (playersGame.length < 2){
+            alert("Adicione Jogadores para começar a Jogar!!")
+        }
+
+
+        
+    })
+})
 
 
 
