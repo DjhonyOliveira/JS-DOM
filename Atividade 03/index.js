572c249648d67c74cb30102e8cf3ddbe3238d0f7
DjhonyMaga <djonatandeoliveira@hotmail.com>
2023-05-26 22:41:21 -0300

    seletorJs

--- Atividade 03/index.js
+++ Atividade 03/index.js
@@ -1,5 +1,6 @@
 const init = document.querySelector('#init')
 const span = document.querySelectorAll('span')
+const nextPlayer = document.querySelector('.next-player')
 
 
 let playersGame = []
@@ -15,28 +16,38 @@ init.addEventListener('click', function(ev){
     const playerTwo = document.getElementById('player-two').value
     const peao2 = document.querySelector('.player2 .peao-player .peao:checked').value
 
-    if (peao1.length = 2){
-        alert('Selecione apenas um peão por jogador!!')
-    } else if (peao2.length = 2) {
-        alert('Selecione apenas um peão por jogador!!')
-    }
-
     // push array
     playersGame.push({playerOne, peao1}, {playerTwo, peao2})
-})
 
+    if (playersGame.length >= 2) {
+        if (playersGame[0].peao1 == 'X') {
+            nextPlayer.value =playersGame[0].playerOne
+        } else {
+            nextPlayer.value = playersGame[1].playerTwo
+        }
+    }
+});
 
 span.forEach(function(quadro){
+
     quadro.addEventListener('click', function(){
 
         if (playersGame.length < 2){
             alert("Adicione Jogadores para começar a Jogar!!")
         }
 
-
-        
+        if (nextPlayer.value === playersGame[0].playerOne){
+            quadro.innerText = playersGame[0].peao1
+            quadro.style.color = '#ff0000'
+            nextPlayer.value = playersGame[1].playerTwo
+        } else {
+            quadro.innerText = playersGame[1].peao2
+            quadro.style.color = '#0000ff'
+            nextPlayer.value = playersGame[0].playerOne
+        }
+       
     })
-})
+});
 
 
 
